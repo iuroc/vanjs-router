6653640b0c741b125ddf36a0a8a200767eb26067
欧阳鹏 <61752998+iuroc@users.noreply.github.com>
2024-01-09 13:09:46 +0800

    Update vanjs-router-1.1.9.nomodule.js

--- js/vanjs-router-1.1.9.nomodule.js
+++ js/vanjs-router-1.1.9.nomodule.js
@@ -1,46 +1,17 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
-import van from 'vanjs-core';
-var div = van.tags.div;
+const { div } = van.tags;
 /** 从 `location.hash` 获取当前路由 */
-var nowRoute = function () {
-    var _a;
-    var li = location.hash.split('/');
-    var route = { name: (_a = li[1]) !== null && _a !== void 0 ? _a : 'home', args: li.slice(2) };
+const nowRoute = () => {
+    const li = location.hash.split('/');
+    const route = { name: li[1] ?? 'home', args: li.slice(2) };
     return route;
 };
-var activeRoute = van.state(nowRoute());
-window.addEventListener('hashchange', function () {
+const activeRoute = van.state(nowRoute());
+window.addEventListener('hashchange', () => {
     activeRoute.val = nowRoute();
 });
-var Route = function (config) {
-    var rest = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        rest[_i - 1] = arguments[_i];
-    }
-    var name = config.name, onFirst = config.onFirst, onLoad = config.onLoad, otherProp = __rest(config, ["name", "onFirst", "onLoad"]);
-    var firstLoad = true;
-    van.derive(function () {
+const Route = (config, ...rest) => {
+    let firstLoad = true;
+    van.derive(() => {
         if (activeRoute.val.name == config.name) {
             if (firstLoad && config.onFirst) {
                 config.onFirst(activeRoute.val);
@@ -50,22 +21,17 @@ var Route = function (config) {
                 config.onLoad(activeRoute.val);
         }
     });
-    return div(__assign({ hidden: function () { return config.name != activeRoute.val.name; } }, otherProp), rest);
+    return div({ hidden: () => config.name != activeRoute.val.name }, rest);
 };
-var routeTo = function (name, args) {
-    if (name === void 0) { name = 'home'; }
-    if (args === void 0) { args = []; }
+const routeTo = (name = 'home', args = []) => {
     if (args.length == 0) {
         if (name == 'home') {
             location.hash = '';
             history.replaceState(null, '', './');
         }
         else
-            location.hash = "/".concat(name);
+            location.hash = `/${name}`;
     }
     else
-        location.hash = "/".concat(name, "/").concat(args.join('/'));
+        location.hash = `/${name}/${args.join('/')}`;
 };
-// export { Route, routeTo }
-Object.defineProperty(window, 'Route', Route);
-Object.defineProperty(window, 'routeTo', routeTo);
