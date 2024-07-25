dde3e4e1d3c31e7da39706782d1ecbb7b9756a57
iuroc <m@iuroc.com>
2024-07-26 03:03:56 +0800

    ..

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,39 @@
 import van from 'vanjs-core'
-import { Handler } from './router'
+import { Route, goto } from './router'
 
 const { div } = van.tags
 
+const Home = () => Route({
+    rule: 'home',
+    Loader() {
+        return div({
+            onclick() {
+                goto('about', 1, 2, 3, 4, 5)
+            }
+        }, 'Hello World')
+    },
+    async onFirst() {
+
+    },
+    async onLoad() {
+    }
+})
+
+const About = () => Route({
+    rule: 'about',
+    Loader() {
+        return div({ style: 'color: skyblue;' }, 'Hello About')
+    },
+    onLoad() {
+    },
+})
+
+const App = () => {
+
+    return div(
+        Home(),
+        About(),
+    )
+}
+
+van.add(document.body, App())
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import van, { TagFunc } from 'vanjs-core'
+import van from 'vanjs-core'
 
 export const nowHash = () => location.hash ? location.hash.slice(1) : 'home'
 
@@ -8,49 +8,73 @@ window.addEventListener('hashchange', event => {
     now.val = nowHash()
 })
 
+export type HandlerConfig = {
+    /** 当前路由的命中规则
+     * - 如果传入字符串，则只匹配 Hash 以 `/` 分割后的首项，其余项作为 `args`。
+     * - 如果传入正则，则匹配正则，捕获分组作为 `args`。
+     * 
+     * 例如：
+     * ```ts
+     * { rule: 'about' }
+     * { rule: /list_(\d+)/ }
+     * ```
+     */
+    rule: Handler['rule']
+    /** 当前路由页面的构建方法 */
+    Loader: Handler['Loader']
+    /** 延迟加载页面，需要显示调用 `show()` 方法显示页面。 */
+    delayed?: Handler['delayed']
+    /** 首次路由命中事件，在 `onLoad` 之前执行。 */
+    onFirst?: Handler['onFirst']
+    /** 路由命中事件，在 `await onFirst()` 之后执行。 */
+    onLoad?: Handler['onLoad']
+}
+
 export class Handler {
     private rule: string | RegExp
-    private args: string[] = []
-    private Loader: TagFunc<HTMLElement>
+    /** 当前路由被命中后接收到的路由参数 */
+    public args: string[] = []
+    private Loader: (this: Handler) => HTMLElement
     private delayed = false
-    private onFirst: (handler: Handler) => Promise<any>
-    private onLoad: (handler: Handler) => Promise<any>
-    private hidden = van.state(true)
+    private onFirst: (this: Handler) => any
+    private onLoad: (this: Handler) => any
+    /** 路由根元素，对外导出，可直接添加到 DOM 树 */
     public element: HTMLElement
+    /** 记录当前是否初次命中路由，决定是否执行 `onFirst` 事件。 */
     private isFirstLoad = true
 
-    constructor(config: {
-        /** 匹配规则
-         * - 如果传入字符串，则只匹配 Hash 以 `/` 分割后的首项，其余项作为 `args`。
-         * - 如果传入正则，则匹配正则，捕获分组作为 `args`。
-         */
-        rule: Handler['rule']
-        /** 元素加载器 */
-        Loader: Handler['Loader']
-        /** 延迟加载页面，需要显示调用 `show()` 方法显示页面。 */
-        delayed?: Handler['delayed']
-        onFirst?: Handler['onFirst']
-        onLoad?: Handler['onLoad']
-    }) {
+    constructor(config: HandlerConfig) {
+        // 载入路由的基础配置
         this.rule = config.rule
         this.Loader = config.Loader
         this.delayed = config.delayed || false
         this.onFirst = config.onFirst || (async () => { })
         this.onLoad = config.onLoad || (async () => { })
+        // 创建页面元素
         this.element = this.Loader()
-        van.derive(() => this.element.hidden = this.hidden.val)
+        this.element.hidden = true
+        // 根据 Hash 的变化，自动更新路由状态
         van.derive(() => {
-            if (!this.matchHash()) {
-                this.update()
+            // 获取当前路由的命中状态
+            const match = this.matchHash()
+            const hidden = !match
+            if (!match) {
+                // 未被命中，刷新路由，页面隐藏。
+                this.hide()
             } else {
-                (async _ => {
+                // 路由命中
+                const func = async () => {
+                    // 将接收到的路由参数保存起来
+                    this.args.splice(0)
+                    this.args.push(...match.args)
                     if (this.isFirstLoad) {
                         this.isFirstLoad = false
-                        await this.onFirst(this)
+                        await this.onFirst()
                     }
-                    await this.onLoad(this)
-                    if (!this.delayed) this.update()
-                })(now.val)
+                    await this.onLoad()
+                    if (!this.delayed) this.show()
+                }
+                func()
             }
         })
     }
@@ -66,7 +90,23 @@ export class Handler {
         return parts[0] == this.rule ? { hash: now.val, args: parts.slice(1) } : false
     }
 
-    public update() {
-        this.hidden.val = !this.matchHash()
+    public show() {
+        this.element.hidden = false
     }
+
+    public hide() {
+        this.element.hidden = true
+    }
+}
+
+/** 创建一个自动管理路由状态的 DOM 元素 */
+export const Route = (config: HandlerConfig) => new Handler(config).element
+
+/**
+ * 
+ * @param name 需要前往的路由名称，对应字符串类型的 rule
+ * @param args 路由参数
+ */
+export const goto = (name: string, ...args: any[]): void => {
+    location.hash = `/${[name, ...args].join('/')}`
 }
